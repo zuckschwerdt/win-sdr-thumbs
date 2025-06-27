c3f72446253958f4f9b28e9f355754ce107a25fc
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2025-06-26 22:12:49 -0700

    Version, gitignore, mame

--- Build.bat
+++ Build.bat
@@ -11,14 +11,14 @@ if %errorLevel% neq 0 (
 )
 
 :: Unregister the previous DLL version
-regsvr32 /u "%~dp0target\x86_64-pc-windows-msvc\release\svg_thumbnails_rust.dll"
+regsvr32 /u "%~dp0target\x86_64-pc-windows-msvc\release\win_svg_thumbs.dll"
 
 :: (Requires IObit Unlocker to be installed) We can't delete the DLL if it's in use, which is often the case even after unregistering it. So this will unlock it and delete it.
-"C:\Program Files (x86)\IObit\IObit Unlocker\IObitUnlocker.exe" /Delete /Normal "%~dp0target\x86_64-pc-windows-msvc\release\svg_thumbnails_rust.dll"
+"C:\Program Files (x86)\IObit\IObit Unlocker\IObitUnlocker.exe" /Delete /Normal "%~dp0target\x86_64-pc-windows-msvc\release\win_svg_thumbs.dll"
 
 :: Run the build. Tried to to make it run as a non-admin, but couldn't get it to work.
 cd /d %~dp0
 cargo build --release --target=x86_64-pc-windows-msvc
 
 :: Re-register the new DLL version
-regsvr32 "%~dp0target\x86_64-pc-windows-msvc\release\svg_thumbnails_rust.dll"
+regsvr32 "%~dp0target\x86_64-pc-windows-msvc\release\win_svg_thumbs.dll"
