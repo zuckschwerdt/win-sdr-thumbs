1b9dce150fb2fc2a177c0a33fdf33070b3b769b8
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2025-06-26 18:18:10 -0700

    Add Build.bat, Include debug symbols

---
+++ Build.bat
@@ -0,0 +1,24 @@
+@echo off
+
+:: Check if running as administrator
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo This script must be run as Administrator.
+    echo Please right-click on the batch file and select "Run as administrator".
+    echo.
+    pause
+    exit /b 1
+)
+
+:: Unregister the previous DLL version
+regsvr32 /u "%~dp0target\x86_64-pc-windows-msvc\release\svg_thumbnails_rust.dll"
+
+:: (Requires IObit Unlocker to be installed) We can't delete the DLL if it's in use, which is often the case even after unregistering it. So this will unlock it and delete it.
+"C:\Program Files (x86)\IObit\IObit Unlocker\IObitUnlocker.exe" /Delete /Normal "%~dp0target\x86_64-pc-windows-msvc\release\svg_thumbnails_rust.dll"
+
+:: Run the build. Tried to to make it run as a non-admin, but couldn't get it to work.
+cd /d %~dp0
+cargo build --release --target=x86_64-pc-windows-msvc
+
+:: Re-register the new DLL version
+regsvr32 "%~dp0target\x86_64-pc-windows-msvc\release\svg_thumbnails_rust.dll"
